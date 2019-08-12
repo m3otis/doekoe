0bfeeb9658d975578dc8c5a41edb5ea98f582fb8
Mitchell Sitohang <m.sitohang@mmguide.nl>
2019-08-12 17:44:30 +0200

    added services etc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,20 +9,27 @@ import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { OverviewComponent } from './components/overview/overview.component';
 import { HomeComponent } from './components/home/home.component';
+import { EntryService } from './services/entry.service';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ToastComponent } from './components/toast/toast.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     OverviewComponent,
-    HomeComponent
+    HomeComponent,
+    ToastComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SlimLoadingBarModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    SlimLoadingBarModule,
+    NgbModule
   ],
-  providers: [],
+  providers: [EntryService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/overview/overview.component.ts
+++ src/app/components/overview/overview.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter } from '@angular/core';
+import { EntryService } from 'src/app/services/entry.service';
+import Entry from 'src/app/models/entry';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-overview',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./overview.component.css']
 })
 export class OverviewComponent implements OnInit {
+  @Input() entries: Entry[];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  calculateTotal(): number {
+    if (this.entries !== undefined) {
+      let amount = 0;
+
+      this.entries.forEach(e => {
+        amount = amount + e.amount;
+      });
+
+      return amount;
+    }
   }
-
 }
